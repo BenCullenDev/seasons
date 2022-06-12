1f0e042554c432b8207e70e76c54f903dada1880
Ben Cullen <benjaminjcullen1@gmail.com>
2022-06-12 20:41:12 +0100

    added lat and month logic and display render accordingly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
 
@@ -15,16 +16,13 @@ class App extends React.Component {
         );
     }
 
-    componentDidUpdate() {
-        console.log('The component was updated - therefore rerendered')
-    }
     render() {
         if (this.state.errorMessage && !this.state.lat) {
         return <div>Error: {this.state.errorMessage}</div>
     }
 
         if (this.state.lat && !this.state.errorMessage) {
-            return <div>Lattitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
     
         return <div>Loading...</div>
