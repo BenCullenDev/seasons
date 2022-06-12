7734b03b6e167bc6fc35e317a4f44c1de4fb948c
Ben Cullen <benjaminjcullen1@gmail.com>
2022-06-12 18:38:40 +0100

    initialised state using constructor and super

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
     render() {
         window.navigator.geolocation.getCurrentPosition(
             postion => console.log(postion),
