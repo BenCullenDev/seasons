7ad3a662449923cf55866c13cb4045e80165e927
Ben Cullen <benjaminjcullen1@gmail.com>
2022-06-13 10:58:51 +0100

    aditional styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
+import "./App.css";
 
 class App extends React.Component {
 
@@ -17,16 +18,25 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
-        return <div>Error: {this.state.errorMessage}</div>
-    }
-
-        if (this.state.lat && !this.state.errorMessage) {
-            return <SeasonDisplay lat={this.state.lat}/>
+            return <div>Error: {this.state.errorMessage}</div>
         }
     
-        return <Spinner message="Please allow location services."/>
+            if (this.state.lat && !this.state.errorMessage) {
+                return <SeasonDisplay lat={this.state.lat}/>
+            }
+        
+            return <Spinner message="Please allow location services."/>
+    }
+
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
 }
 }
 ReactDOM.render(
